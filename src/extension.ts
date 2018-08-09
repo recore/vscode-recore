cdf2ef3dc2e7d3b43abb90b037439fe9b2b588e7
Albert <albert_zju@163.com>
2018-08-09 22:10:46 +0800

    feat: add component to editor

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { CompNodeProvider } from './components';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -22,6 +23,43 @@ export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "recore" is now active!');
+
+  const rootPath = vscode.workspace.rootPath || '';
+	const componentsProvider = new CompNodeProvider(rootPath);
+  vscode.window.registerTreeDataProvider('recore-ui-library-uxcore', componentsProvider);
+  vscode.commands.registerCommand('extension.openComponentList', component => {
+    // current editor
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      const selection : vscode.Selection = editor.selection;
+      const { displayName, props } = component;
+
+      let line = selection.active.line;
+      const length = selection.active.character || 0;
+      let insertPos = new vscode.Position(line, length);
+      const prefix = `<${displayName}`;
+
+      const insertProps = (props: any, line: number) => {
+        Object.keys(props).map((key, index) => {
+          const insertPos = new vscode.Position(line + index + 1, 0);
+          editor.insertSnippet(new vscode.SnippetString(`${key}="${props[key]}"\n`), insertPos);
+        });
+      };
+
+      // 换行
+      if (length > 0) {
+        // 插入标签
+        editor.insertSnippet(new vscode.SnippetString('\n'), insertPos);
+        line = line + 1;
+      }
+
+      const propStartInsertPos = new vscode.Position(line, length);
+      editor.insertSnippet(new vscode.SnippetString(`${prefix}\n`), propStartInsertPos);
+      insertProps(props, line);
+      const sufixInsertPos = new vscode.Position(line + Object.keys(props).length + 1, 0);
+      editor.insertSnippet(new vscode.SnippetString('/>'), sufixInsertPos);
+    }
+  });
 }
 
 // this method is called when your extension is deactivated
